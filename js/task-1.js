cacb98938294d5b6cc5481bd1226f9a6100098d3
furkangoit <celikfurkan45@hotmail.com>
2025-04-06 19:32:15 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -2,9 +2,9 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = quantity * pricePerDroid;
 
   if (customerCredits < totalPrice) {
-    return "Insufficient funds!";
+    return 'Insufficient funds!';
   } else {
-    return "You ordered ${quantity} droids worth ${totalPrice} credits!";
+    return 'You ordered ${quantity} droids worth ${totalPrice} credits!';
   }
 }
 
